29cacc43237c1b72a7cb0ea8ccda02c0b5eed9c5
Juan Carlos Ramirez <jc_ramirez2baylor.edu>
2016-10-31 20:31:01 -0500

    I'll dick with this later

--- Enigo's Music Visualizer/Enigo's Music Visualizer/main.cpp
+++ Enigo's Music Visualizer/Enigo's Music Visualizer/main.cpp
@@ -10,16 +10,32 @@
 #include <SFML/Audio.hpp>
 #include <SFML/OpenGL.hpp>
 
+#include <vector>
 
 int main() {
     
     sf::RenderWindow window;
-    window.create(sf::VideoMode(300, 300), "Enigo's Music Visualizer");
+    window.create(sf::VideoMode(1600, 900), "Enigo's Music Visualizer");
 
     sf::InputSoundFile mySound;
-    mySound.openFromFile("test.mp3");
-    std::cout << mySound.getSampleCount() << std::endl;
+    mySound.openFromFile("puzzle_loop.wav");
+    std::vector<sf::Int16> sampleStorage;
 
+    //Loading the by seconds sound information
+    for (float i = 0; i < mySound.getDuration().asSeconds(); i++) {
+        mySound.seek(sf::Time(sf::seconds(1+i)));
+        sf::Int16 sample;
+        mySound.read(&sample, 1);
+        sampleStorage.push_back(sample);
+    }
+
+    //Playing music information
+    sf::Music sound;
+    sound.openFromFile("puzzle_loop.wav");
+    sound.play();
+    sound.setLoop(false);
+
+    //Render loop
     while (window.isOpen()) {
         sf::Event e;
         while (window.pollEvent(e)) {
@@ -38,6 +54,16 @@ int main() {
         }
 
         window.clear();
+
+        //Line information to draw from audio
+         for(int i = 0; i < sampleStorage.size(); i++){
+
+            sf::RectangleShape myLine;
+            myLine.setSize(sf::Vector2f(window.getSize().x / sampleStorage.size(),sampleStorage.at(i)));
+            myLine.setFillColor(sf::Color::Red);
+            myLine.setPosition(i + (window.getSize().x / sampleStorage.size()), (window.getSize().y / 2));
+            window.draw(myLine);
+        }
         window.display();
 
     }
