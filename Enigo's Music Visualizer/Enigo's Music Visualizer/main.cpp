81d13fd0460cb2bb13e2e05d1a36d9a212a80556
Juan Carlos Ramirez <jc_ramirez2baylor.edu>
2016-10-31 22:00:28 -0500

    Need to make it work with oggs, kinda works with wavs.

--- Enigo's Music Visualizer/Enigo's Music Visualizer/main.cpp
+++ Enigo's Music Visualizer/Enigo's Music Visualizer/main.cpp
@@ -12,28 +12,52 @@
 
 #include <vector>
 
+
 int main() {
     
+    sfg::SFGUI GUI;
+    sfg::Desktop desktop;
+
     sf::RenderWindow window;
     window.create(sf::VideoMode(1600, 900), "Enigo's Music Visualizer");
 
     sf::InputSoundFile mySound;
-    mySound.openFromFile("puzzle_loop.wav");
+    mySound.openFromFile("nice_music.ogg");
     std::vector<sf::Int16> sampleStorage;
 
     //Loading the by seconds sound information
     for (float i = 0; i < mySound.getDuration().asSeconds(); i++) {
         mySound.seek(sf::Time(sf::seconds(1+i)));
         sf::Int16 sample;
-        mySound.read(&sample, 1);
+        if (mySound.read(&sample, 1) < 1) {
+            std::fprintf(stderr,"Did not read the correct sample size\n");
+            
+            return -1;
+        }
         sampleStorage.push_back(sample);
     }
 
     //Playing music information
     sf::Music sound;
-    sound.openFromFile("puzzle_loop.wav");
+    sound.openFromFile("nice_music.ogg");
     sound.play();
-    sound.setLoop(false);
+    sound.setLoop(true);
+
+    //Line information to draw from audio
+    sf::VertexArray mySeconds(sf::LineStrip);
+    for (int i = 0; i < sampleStorage.size(); i++) {
+
+        sf::Vertex mySecond;
+        mySecond.position = sf::Vector2f(i * (window.getSize().x / sampleStorage.size()), (window.getSize().y / 2.0) + ((sampleStorage.at(i) - -(window.getSize().y / 2.0)) / window.getSize().y));
+        if (std::floor(sound.getPlayingOffset().asSeconds()) == i) {
+            mySecond.color = (sf::Color::Red);
+        }
+        else {
+            mySecond.color = (sf::Color::White);
+        }
+
+        mySeconds.append(mySecond);
+    }
 
     //Render loop
     while (window.isOpen()) {
@@ -54,16 +78,13 @@ int main() {
         }
 
         window.clear();
+        
+        int currentVerxtex = std::floor(sound.getPlayingOffset().asSeconds());
+        mySeconds[currentVerxtex].color = sf::Color::Red;
+       
+        window.draw(mySeconds);
 
-        //Line information to draw from audio
-         for(int i = 0; i < sampleStorage.size(); i++){
-
-            sf::RectangleShape myLine;
-            myLine.setSize(sf::Vector2f(window.getSize().x / sampleStorage.size(),sampleStorage.at(i)));
-            myLine.setFillColor(sf::Color::Red);
-            myLine.setPosition(i + (window.getSize().x / sampleStorage.size()), (window.getSize().y / 2));
-            window.draw(myLine);
-        }
+        mySeconds[currentVerxtex].color = sf::Color::White;
         window.display();
 
     }
